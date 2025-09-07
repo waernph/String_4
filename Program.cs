8916db0487fe774c9fd0e1e7f8dba21a7de72447
waernph <102241220+waernph@users.noreply.github.com>
2025-09-07 14:05:51 +0200

    final fixing

--- Program.cs
+++ Program.cs
@@ -5,16 +5,13 @@ public class Program
     public static void Main()
     {
         bool goOn = true;
-        string password = "";
-        object register;
         while (goOn)
         {
             Console.Clear();
             Console.Write("Skriv in för- och efternamn: ");
             string correctNaming = Capital.MakeInitialCaseToUpper(Console.ReadLine());
-            Console.Write("Skriv in lösenord: ");
-            password = Console.ReadLine();
-            register = Capital.ToDict(correctNaming, password);
+            Console.WriteLine($"Jag har fixat ditt namn: {correctNaming}");
+
             Console.ReadKey();
 
         }
--- capital.cs
+++ capital.cs
@@ -13,6 +13,9 @@ public class Capital
         for (var i = 0; i < length; i++)
         {
             char first = char.ToUpper(words[i][0]);
+            Console.WriteLine(words[i]);
+            //words[i] = words[i].Replace(words[i][0], first);
+            //words[i] = words[i].Replace(words[i][0], first);
             words[i] = words[i].Remove(0, 1);
             output += first + words[i] + " ";
         }
